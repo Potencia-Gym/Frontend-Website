eef5c66e017c933674cc66cc4489601644a88b0b
Darsh Kejriwal <c-darsh@aulacube.com>
2023-11-13 21:15:07 +0530

    removed lazy loading from auth images

--- src/Auth/signup.jsx
+++ src/Auth/signup.jsx
@@ -7,7 +7,7 @@ function signup() {
     <div className="flex flex-col md:flex-row h-screen items-center bg-black font-font1">
 
       <div className="bg-green hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
-        <img src={signUpImg} alt="pic" loading='lazy' className="w-full h-full object-cover" />
+        <img src={signUpImg} alt="pic" className="w-full h-full object-cover" />
       </div>
 
       <div className="bg-black text-white w-full md:max-w-md lg:max-w-full md:mx-auto md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
