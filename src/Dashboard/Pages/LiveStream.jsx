0d97a9ed4638f81ef57a2b078773a587affe0962
Darsh Kejriwal <c-darsh@aulacube.com>
2024-03-09 23:54:31 +0530

    done streaming canvas

--- src/Dashboard/Pages/LiveStream.jsx
+++ src/Dashboard/Pages/LiveStream.jsx
@@ -6,70 +6,30 @@ const LiveStream = () => {
 	const videoRef = useRef(null);
 	const [isConnected, setIsConnected] = useState(socket.connected);
 	const [data, setData] = useState(null);
-
-
-	const getCam = async () => {
-		const stream = await navigator.mediaDevices.getUserMedia({ video: true })
-		const mediaRecorder = new MediaRecorder(stream);
-		videoRef.current.srcObject = stream;
-		mediaRecorder.ondataavailable = async (event) => {
-			const formData = new FormData();
-			const blob = new Blob([event.data], { type: '' });
-			formData.append('frame', event.data);
-
-			// Append the Blob data to the FormData object
-			// Send the FormData object over the WebSocket connection
-			// socket.emit('stream', formData);
-			// console.log("captured", (event.data).toString());
-			// socket.emit('stream', JSON.stringify(event.data));
-		}
-		mediaRecorder.start(1000);
-	}
+	const canvasRef = useRef(null);
+	const [stream, setStream] = useState(null);
 
 	useEffect(() => {
-		getCam();
-	}, [])
-
-	useEffect(() => {
-		// navigator.mediaDevices.getUserMedia({ video: true })
-		// 	.then((stream) => {
-		// 		const videoTrack = stream.getVideoTracks()[0];
-		// 		videoRef.current.srcObject = stream;
-
-		// 		// Create a MediaRecorder to capture video data
-		// 		const options = {}; // Adjust mimeType as needed
-		// 		const mediaRecorder = new MediaRecorder(stream, options);
-
-		// 		// Listen for errors
-		// 		mediaRecorder.onerror = (event) => {
-		// 			console.error('MediaRecorder error:', event.error);
-		// 		};
-		// 		mediaRecorder.ondataavailable = (event) => {
-		// 			// Send the Blob data over the WebSocket connection
-		// 			socket.emit('stream', event.data);
-		// 		};
-		// 		// Start recording video
-		// 		mediaRecorder.start(0);
-		// 	})
-		// 	.catch((error) => {
-		// 		console.error('Error accessing media devices:', error);
-		// 	});
-
-
-
-		const handleConnect = () => {
+		socket.connect();
+		navigator.mediaDevices
+			.getUserMedia({ video: true })
+			.then((stream) => {
+				videoRef.current.srcObject = stream;
+				setStream(stream);
+			})
+		function handleConnect() {
+			console.log("fsfse");
 			setIsConnected(true);
-		};
+		}
 
-		const handleDisconnect = () => {
+		function handleDisconnect() {
 			setIsConnected(false);
-		};
+		}
 
-		const handleMessage = (data) => {
+		function handleMessage(data) {
 			console.log(data);
-		};
-
-		// Establish WebSocket connection
+		}
+		//stablish WebSocket connection
 		socket.on('connect', handleConnect);
 		socket.on('disconnect', handleDisconnect);
 		socket.on('message', handleMessage);
@@ -80,18 +40,53 @@ const LiveStream = () => {
 			socket.off('disconnect', handleDisconnect);
 			socket.off('message', handleMessage);
 		};
-
-		// Capture media stream
-
-
 	}, []);
 
+	const capture = () => {
+		const video = videoRef.current
+		const canvas = canvasRef?.current;
+		const ctx = canvas?.getContext('2d');
+
+		// Set the canvas dimensions to control the quality
+		canvas.width = 900; // Adjust as needed
+		canvas.height = 700; // Adjust as needed
+
+		// Draw the current video frame onto the canvas
+		ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+		const imgurl = canvas.toDataURL('image/jpeg', 0.8);
+		socket.emit('send_frame', imgurl);
+		// 		const mediaRecorder = new MediaRecorder(stream);
+		// 		console.log("mediaRecorder : ", mediaRecorder);
+		// 		mediaRecorder.start(10000);
+		// 		mediaRecorder.ondataavailable = (e) => {
+		// 			var reader = new FileReader();
+		// 			reader.readAsDataURL(e.data);
+		// 			reader.onloadend = function () {
+		// 				var base64data = reader.result;
+		// 				setData(base64data);
+		// 				// console.log(base64data);
+		// 				// stream(socket).emit('stream', stream, base64data);
+		// 				// socket.emit('stream', base64data)
+		// 			}
+		// 		};
+		// 		mediaRecorder.onstart = () => {
+		// 			console.log('recording started')
+		// 		}
+		// 	})
+	}
+
+	useEffect(() => {
+		setInterval(() => {
+			capture();
+		}, 100);
+	}, [])
 
 	return (
 		<div>
-			<h1>Connection to server: {isConnected ? 'true' : 'false'} {socket?.id}</h1>
+			<h1>Connection to server: {isConnected ? 'true' : 'false'}</h1>
 			<video ref={videoRef} autoPlay />
-			<div className="">DATA : {data?.sid}</div>
+			<canvas ref={canvasRef}></canvas>
+			<div className="">DATA : {socket.id}</div>
 		</div>
 	)
 }
--- src/routing/router.jsx
+++ src/routing/router.jsx
@@ -4,9 +4,7 @@ import Dashboard from '../Dashboard/MainDash'
 import { Login, Signup } from "../Auth";
 import WorkoutPlan from "../Dashboard/Pages/WorkoutPlan";
 import DailyGoal from "../Dashboard/Pages/DailyGoal";
-import TrackProgress from "../Dashboard/Pages/TrackProgress";
 import DietPlanner from "../Dashboard/Pages/DietPlanner";
-import ExercisePlanner from "../Dashboard/Pages/ExercisePlanner";
 import LiveStream from "../Dashboard/Pages/LiveStream";
 
 const router = createBrowserRouter([
@@ -33,18 +31,6 @@ const router = createBrowserRouter([
             {
                 path: 'daily-goal',
                 element: <DailyGoal />,
-            },
-            {
-                path: 'track-progress',
-                element: <TrackProgress />
-            },
-            {
-                path: 'diet-planner',
-                element: <DietPlanner />
-            },
-            {
-                path: 'exercise-planner',
-                element: <ExercisePlanner />
             },
         ]
     },
